37f11e5a45326a5d3296f1ba78343043bfff7046
MShultz <mshultz@student.neumont.edu>
2016-07-31 13:55:37 -0600

    Complete User Search Program.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String fileName = "src/Indexed.bin";
+		UserInterface ui = new UserInterface(fileName);
+		ui.searchEngine();
+
+	}
+
+}
